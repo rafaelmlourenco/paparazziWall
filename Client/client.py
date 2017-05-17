35ea0e44074764084a3166df9294b9e97e911d20
Rafael Lourenço <rafaelmlourenco@gmail.com>
2017-05-17 17:23:49 +0100

    Corrected controller_program.ino to be usable on Arduino Micro; client.py now allows to define a duration using -t argument

--- Client/client.py
+++ Client/client.py
@@ -1,5 +1,21 @@
+import sys
 import socket
 HOST = 'localhost'    # The remote host
-PORT = 50997              # The same port as used by the server
+PORT = 50997          # The same port as used by the server
+
+flashDuration = 10000
+argsLength = len(sys.argv)
+if argsLength > 1:
+    i = 0
+    while i < argsLength:
+        if sys.argv[i] == "-t":
+            if i+1 < argsLength:
+                i = i+1
+                flashDuration = int(sys.argv[i])
+            else:
+                print("-t option requires a duration (milliseconds)")
+        i = i+1
+
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((HOST, PORT))
+    s.sendall(bytes("time:" + str(flashDuration)))
--- controller_program/controller_program.ino
+++ controller_program/controller_program.ino
@@ -16,11 +16,12 @@ int STOP_STATUS_DURATION = 150;
 int ERROR_SERIAL = 2;
 int ERROR_SERIAL_DURATION = 500;
 
-unsigned long FLASHING_DURATION = 10000; 
+unsigned long FLASHING_DURATION = 10000;
 
 int switchState = 0;
 bool flashState = true;
 bool lastState = false;
+bool isConnected = true;
 
 unsigned long startTime = 0;
 unsigned long currentTime = 0;
@@ -29,7 +30,6 @@ String START_FLASHING_MSG = "Start Flashing";
 String READY_MSG = "Arduino is ready!";
 
 String rcvString;
-
 
 void setup() {
   //Initialize Serial Port (USB)
@@ -45,9 +45,9 @@ void setup() {
   pinMode(FLASH_06, OUTPUT);
   pinMode(FLASH_07, OUTPUT);
   pinMode(CAMERA_SHUTTER, OUTPUT);
-  
+
   // Control button (stop button)
-  pinMode(CONTROL_BUTTON, INPUT);
+  //  pinMode(CONTROL_BUTTON, INPUT);
 
   // if analog input pin 0 is unconnected, random analog
   // noise will cause the call to randomSeed() to generate
@@ -55,29 +55,23 @@ void setup() {
   // randomSeed() will then shuffle the random function.
   randomSeed(analogRead(0));
   blinkStatusLED(INIT_STATUS, INIT_STATUS_DURATION);
-  Serial.println(READY_MSG);
 }
 
 void loop() {
+  while (!Serial) {
+    // Do nothing... Wait for serial connection...
+    isConnected = false;
+  }
+
   // put your main code here, to run repeatedly:
-  switchState = digitalRead(CONTROL_BUTTON);
-  
-  if (switchState == HIGH) {
-    flashState = false;
-    blinkStatusLED(STOP_STATUS, STOP_STATUS_DURATION);
-    startTime = 0;
+  if (!isConnected) {
+    isConnected = true;
+    Serial.println(READY_MSG);
   }
 
-  if(lastState == false && flashState == true){
-    startTime = millis();
-  }
-
-  currentTime = millis();
-
-  if(currentTime - startTime >= FLASHING_DURATION){
-    flashState = false;
-  }
-
+  // readControlSwitch();
+  updateFlashStateWithTimeCheck();
+ 
   if (Serial.available() > 0) {
     handleSerialMsg();
   }
@@ -87,25 +81,49 @@ void loop() {
     flashSingleRandomly(randomDuration);
   }
 
+  // Iterate (next time the lastState will be the current one)
   lastState = flashState;
-} // go back to the beginning of the loop
+  } // go back to the beginning of the loop
+
+void updateFlashStateWithTimeCheck(){
+  if (lastState == false && flashState == true) {
+    startTime = millis();
+  }
+
+  currentTime = millis();
+
+  if (currentTime - startTime >= FLASHING_DURATION) {
+    flashState = false;
+    digitalWrite(STATUS_LED, LOW);
+  }
+}
+
+void readControlSwitch() {
+  //  switchState = digitalRead(CONTROL_BUTTON);
+  //
+  //  if (switchState == HIGH) {
+  //    flashState = false;
+  //    blinkStatusLED(STOP_STATUS, STOP_STATUS_DURATION);
+  //    startTime = 0;
+  //  }
+}
 
 void flashSingleRandomly(int duration) {
   int portNumber = random(4, 11);
   digitalWrite(CAMERA_SHUTTER, HIGH);
   digitalWrite(portNumber, HIGH);
-  
+
   delay(duration);
-  
+
   digitalWrite(CAMERA_SHUTTER, LOW);
-  digitalWrite(FLASH_01, LOW);
-  digitalWrite(FLASH_02, LOW);
-  digitalWrite(FLASH_03, LOW);
-  digitalWrite(FLASH_04, LOW);
-  digitalWrite(FLASH_05, LOW);
-  digitalWrite(FLASH_06, LOW);
-  digitalWrite(FLASH_07, LOW);
- 
+  digitalWrite(portNumber, LOW);
+//  digitalWrite(FLASH_02, LOW);
+//  digitalWrite(FLASH_03, LOW);
+//  digitalWrite(FLASH_04, LOW);
+//  digitalWrite(FLASH_05, LOW);
+//  digitalWrite(FLASH_06, LOW);
+//  digitalWrite(FLASH_07, LOW);
+
   delay(duration);
 }
 
